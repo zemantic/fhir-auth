23c8b5bbf245974574eb173ff5ce6b74eead3f53
Rukshan <arkruka@gmail.com>
2022-11-04 19:07:10 +0530

    create client and get client by id routes added

---
+++ src/routes/clients.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { createClient, getClientById } from "../controllers/clients";
+
+const route = Router();
+
+route.post("/client", async (req, res, next) => {
+  const clientName: string = req.body.clientName;
+  const clientHost: string = req.body.clientHost;
+  const clientPublicKeyEndpoint: string = req.body.clientPublicKeyEndpoint;
+  const userId = 1;
+
+  const request = await createClient(
+    clientName,
+    clientHost,
+    clientPublicKeyEndpoint,
+    userId
+  );
+  return res.status(request.status).json(request);
+});
+
+route.get("/client/:id", async (req, res, next) => {
+  const id: string = req.params.id;
+  const request = await getClientById(Number(id));
+
+  return res.status(request.status).json(Number(request.data?.client.id));
+});
+
+export { route as clientRoutes };
