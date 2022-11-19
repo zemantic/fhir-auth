f606cee5d21f0619b400a7ee985dc597692f99a0
Rukshan <arkruka@gmail.com>
2022-11-20 02:41:17 +0530

    added authentication to remaining fhir routes

--- src/routes/fhir.ts
+++ src/routes/fhir.ts
@@ -3,65 +3,103 @@ import { verifyJwt, verifyScopes } from "../auth/authentication";
 
 const route = Router();
 
-route.get(
-  "/fhir/:resource/:id",
-  async (req, res, next) => {
-    const token = req.headers.authorization;
-    const verify = await verifyJwt(token);
-    if (verify.status !== 200) {
-      return res.status(verify.status).json(verify);
-    }
-    res.locals.scopes = verify.data.scopes;
-    res.locals.clientId = verify.data.clientId;
-    next();
-  },
-  async (req, res, next) => {
-    const verfiyScopes = await verifyScopes(res.locals.scopes);
-    const checkResource = verfiyScopes.findIndex(
-      (privilage) =>
-        privilage.resource.toLowerCase() === req.params.resource.toLowerCase()
-    );
-
-    if (checkResource !== -1 && verfiyScopes[checkResource].privilages.read) {
-      // TODO: ADD FETCH REQUEST TO FHIR SERVER
-      return res.status(200).send(req.query);
-    }
-
-    return res.status(403).send(req.query);
+route.all("/fhir/*", async (req, res, next) => {
+  const token = req.headers.authorization;
+  const verify = await verifyJwt(token);
+  if (verify.status !== 200) {
+    return res.status(verify.status).json(verify);
   }
-);
-
-route.get("/fhir/:resource", (req, res, next) => {
-  return res.json({});
+  res.locals.scopes = verify.data.scopes;
+  res.locals.clientId = verify.data.clientId;
+  next();
 });
 
-route.post("/fhir/:resource/_search", (req, res, next) => {});
+route.get("/fhir/:resource/:id?", async (req, res, next) => {
+  const verfiyScopes = await verifyScopes(res.locals.scopes);
+  const checkResource = verfiyScopes.findIndex(
+    (privilage) =>
+      privilage.resource.toLowerCase() === req.params.resource.toLowerCase()
+  );
 
-route.post("/fhir/:resource", (req, res, nex) => {});
+  if (checkResource !== -1 && verfiyScopes[checkResource].privilages.read) {
+    // TODO: ADD FETCH REQUEST TO FHIR SERVER
+    return res.status(200).send(req.query);
+  }
 
-route.put("/fhir/:resource", (req, res, next) => {});
+  return res.status(401).send(req.query);
+});
 
-route.patch("/fhir/:resource", (req, res, next) => {});
+route.post("/fhir/:resource/_search", async (req, res, next) => {
+  const verfiyScopes = await verifyScopes(res.locals.scopes);
+  const checkResource = verfiyScopes.findIndex(
+    (privilage) =>
+      privilage.resource.toLowerCase() === req.params.resource.toLowerCase()
+  );
+  if (checkResource !== -1 && verfiyScopes[checkResource].privilages.search) {
+    // TODO: ADD FETCH REQUEST TO FHIR SERVER
+    return res.status(200).send(req.query);
+  }
+  return res.status(401).send(req.query);
+});
 
-// route.get(
-//   "/fhir/:params",
-//   async (req, res, next) => {
-//     const token = req.headers.authorization;
-//     const verify = await verifyJwt(token);
-//     if (verify.status !== 200) {
-//       return res.status(verify.status).json(verify);
-//     } else {
-//       res.locals.clientId = verify.data.clientsId;
-//       res.locals.scopes = verify.data.scopes;
-//     }
-//     next();
-//   },
-//   (req, res, next) => {
-//     const verfiedScopes = verifyScopes(
-//       res.locals.scopes,
-//       Number(res.locals.clientId)
-//     );
-//   }
-// );
+route.post("/fhir/:resource", async (req, res, nex) => {
+  const verfiyScopes = await verifyScopes(res.locals.scopes);
+  const checkResource = verfiyScopes.findIndex(
+    (privilage) =>
+      privilage.resource.toLowerCase() === req.params.resource.toLowerCase()
+  );
+
+  if (checkResource !== -1 && verfiyScopes[checkResource].privilages.create) {
+    // TODO: ADD FETCH REQUEST TO FHIR SERVER
+    return res.status(200).send(req.query);
+  }
+
+  return res.status(401).send(req.query);
+});
+
+route.put("/fhir/:resource", async (req, res, next) => {
+  const verfiyScopes = await verifyScopes(res.locals.scopes);
+  const checkResource = verfiyScopes.findIndex(
+    (privilage) =>
+      privilage.resource.toLowerCase() === req.params.resource.toLowerCase()
+  );
+
+  if (checkResource !== -1 && verfiyScopes[checkResource].privilages.update) {
+    // TODO: ADD FETCH REQUEST TO FHIR SERVER
+    return res.status(200).send(req.query);
+  }
+
+  return res.status(401).send(req.query);
+});
+
+route.patch("/fhir/:resource", async (req, res, next) => {
+  const verfiyScopes = await verifyScopes(res.locals.scopes);
+  const checkResource = verfiyScopes.findIndex(
+    (privilage) =>
+      privilage.resource.toLowerCase() === req.params.resource.toLowerCase()
+  );
+
+  if (checkResource !== -1 && verfiyScopes[checkResource].privilages.update) {
+    // TODO: ADD FETCH REQUEST TO FHIR SERVER
+    return res.status(200).send(req.query);
+  }
+
+  return res.status(401).send(req.query);
+});
+
+route.delete("/fhir/:resource/id?", async (req, res, next) => {
+  const verfiyScopes = await verifyScopes(res.locals.scopes);
+  const checkResource = verfiyScopes.findIndex(
+    (privilage) =>
+      privilage.resource.toLowerCase() === req.params.resource.toLowerCase()
+  );
+
+  if (checkResource !== -1 && verfiyScopes[checkResource].privilages.delete) {
+    // TODO: ADD FETCH REQUEST TO FHIR SERVER
+    return res.status(200).send(req.query);
+  }
+
+  return res.status(401).send(req.query);
+});
 
 export { route as fhirRoutes };
