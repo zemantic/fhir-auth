8539cb82e05d6ef398520d4c8ff9c017f3a8ba4e
Rukshan <arkruka@gmail.com>
2023-04-15 11:35:38 +0530

    feat: :sparkles: upload fhir resources to server

--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { readResources } from "../controllers/resources";
+import { patchResources, readResources } from "../controllers/resources";
 
 const routes = Router();
 
 routes.get("/resources/:fhirVersion", async (req, res) => {
   const fhirVersion = Number(req.params.fhirVersion);
   const request = await readResources(fhirVersion);
+  return res.status(request.status).json(request);
+});
+
+routes.patch("/resources", async (req, res) => {
+  const fhirResources = req.body.fhirResources;
+  const request = await patchResources(fhirResources);
   return res.status(request.status).json(request);
 });
 
